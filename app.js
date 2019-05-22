2ccf209b455b55e054573964cf34c0d45027d792
JennaKoslowski <jenna.koslowski@yahoo.com>
2019-05-22 06:29:34 -0500

    add makeGrid function- table not appending

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 //global variables to select elements
 let coin=document.getElementsByClassName("coin"); //spinning coin on top
-let turn=document.getElementsByClassName("turn"); //div saying whose turn it is
-let slot=document.getElementsByClassName("slot"); // coin slots
+let turn=document.getElementsByClassName("turn"); //div saying whose turn it is // coin slots
+let board=document.getElementsByClassName("board");
 let reset=document.getElementsByClassName("reset"); //bottom reset button
 let player1;
 let player2;
 var col;
 var occupied;
-console.log(slot);
-slot.addEventListener("click", function(event){
-    bkgdChange();
-});
 
-function bkgdChange(){
-    let turns= 1;
-    if (turns%2==1){
-        slot.style.backgroundColor = "var(--p1)";
-        }
-    else {
-        slot.style.backgroundColor = "var(--p2)";
-    }
-} 
+function makeGrid(){  //create grid upon clicking submit
+ for (let n=0; n<=6; n++){//add row
+     $(".board").append($("<tr></tr>"));
+ for (let m=0; m<7; m++){ //add column to row
+     $("tr").last().append($("<td></td>"));
+}  }  }
+
+
+//board.addEventListener("click", function(colorChange){
+//colorChange.target.style.backgroundColor = "pink";
+// })
+//change color on click
